0f075e9c09021fac5cdfd7c2e5a30c1a4eec9bcd
VatsalRishabh27 <vatsalrishabh00@gmail.com>
2025-05-25 10:09:11 +0530

    sd

--- app/admin/additems/ProductUpdateCard.jsx
+++ app/admin/additems/ProductUpdateCard.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Button } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
+import Image from "next/image";
 
 const ProductUpdateCard = ({ productId, productImg, productName, setProductId }) => {
   // Function to set product ID in the parent component
